6b23b303c1a689fd7832201e931ed7566c6323a2
gkadnar <goran.kadnar@adcon.hr>
2019-03-23 11:06:27 +0100

    rename first program from Program.cs to MyFirstProgram.cs

---
+++ CSharpFundamentals/LanguageBasics/MyFirstProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;                                               // Importing namespace
+
+namespace LanguageBasics                                    // Namespace declaration
+{
+    class Program                                           // Class declaration
+    {
+        static void Main(string[] args)                     // Method declaration
+        {
+            Console.WriteLine(MultiplyBy23(10));            // Statement 1
+            Console.WriteLine(MultiplyBy23(23));            // Statement 2
+        }                                                   // End of method
+
+        static int MultiplyBy23(int number)
+        {
+            return number * 23;
+        }
+
+    }                                                       // End of class
+}                       
